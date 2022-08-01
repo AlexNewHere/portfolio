c8e78b860b408374f0f968399ad97b9b39184da3
Alex <tendren3310@googlemail.com>
2022-08-01 12:43:20 +0300

    add project

--- src/Project/ProjectCard/CardProject.tsx
+++ src/Project/ProjectCard/CardProject.tsx
@@ -1,18 +1,25 @@
 import s from './Card.module.scss';
 import React from 'react';
-import project from '../../logo/project.jpg';
 
+type ProjectsPropsType = {
+    title: string,
+    description: string,
+    url: string,
+    image: string
+}
 
-export function CardProject() {
+export function CardProject({url, title, description, image}:ProjectsPropsType) {
     return (
+        <a href={url} rel="noreferrer" target="_blank">
         <div className={s.card}>
             <div className={s.cardPlace}>
-                <img src={project} alt={'project'}/>
+                <img src={image} alt={'project'}/>
             </div>
             <div className={s.cardInfo}>
-                <h3>React</h3>
-                <p>application</p>
+                <h3>{title}</h3>
+                <h5>{description}</h5>
             </div>
         </div>
+        </a>
     );
 }
--- src/Project/Projects.tsx
+++ src/Project/Projects.tsx
@@ -3,18 +3,52 @@ import s from '../Project/Project.module.scss';
 import cont from '../Common/Style/Container.module.scss';
 import {CardProject} from './ProjectCard/CardProject';
 import Title from '../Common/Title/Title';
+import toDo from '../logo/project/toDo.png'
 
 const Projects = () => {
+    const projects = [
+        {
+            id: 1,
+            title: 'Card',
+            description: 'React / Redux Toolkit / TypeScript / Axios / Formik / MUI / React-router-dom',
+            url: 'https://github.com/AlexNewHere/counter',
+            image: `${toDo}`
+        },
+        {
+            id: 2,
+            title: 'To-Do List',
+            description: 'React / TypeScript / MUI / Storybook / Redux / Redux-Thunk / Axios / Formik / React-router-dom / Unit tests',
+            url: 'https://github.com/AlexNewHere/todo-list',
+            image: `${toDo}`
+        },
+        {
+            id: 3,
+            title: 'Social network',
+            description: 'React / Redux Toolkit / TypeScript / MUI / Axios / React hook form / React-router-dom',
+            url: 'https://github.com/AlexNewHere/samurai-way',
+            image: `${toDo}`
+        },
+        {
+            id: 4,
+            title: 'Counter',
+            description: 'TypeScript / React / Redux / Redux Toolkit',
+            url: 'https://github.com/AlexNewHere/counter',
+            image: `${toDo}`
+        },
+    ]
     return (
         <div className={cont.container + ' ' + s.project}>
             <Title title={'Projects'}/>
             <div className={s.place}>
-                <CardProject/>
-                <CardProject/>
-                <CardProject/>
-                <CardProject/>
-                <CardProject/>
-                <CardProject/>
+                {projects.map(el=> {
+                    return (
+                        <CardProject  key={el.id}
+                                      title={el.title}
+                                      description={el.description}
+                                      url={el.url}
+                                      image={el.image}/>
+                    )
+                })}
             </div>
         </div>
     );
